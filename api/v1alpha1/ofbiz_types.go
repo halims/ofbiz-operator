5f82fb3372fb901ad70b0e20bb8f128c7b407c6b
Halim Sugandi <halimsugandi@gmail.com>
2025-07-01 21:43:35 +0700

    reorganize configuration fields

--- api/v1alpha1/ofbiz_types.go
+++ api/v1alpha1/ofbiz_types.go
@@ -23,6 +23,47 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// OfbizSpec defines the desired state of Ofbiz
+type OfbizSpec struct {
+
+	// The container image for OFBiz.
+	// +kubebuilder:validation:Required
+	Image string `json:"image"`
+
+	// Number of desired pods. Defaults to 1.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=1
+	Size int32 `json:"replicas"`
+
+	// Configuration for the external PostgreSQL database.
+	// +kubebuilder:validation:Required
+	Database DatabaseSpec `json:"database"`
+
+	// If provided, the operator will manage the database, user, and permissions.
+	// +optional
+	// PostgresAdmin *PostgresAdminSpec `json:"postgresAdmin,omitempty"`
+
+	// Web configuration, including SSL.
+	// +optional
+	Web WebSpec `json:"web,omitempty"`
+
+	// Configuration for initial admin account.
+	// +optional
+	InitialAdmin AdminAccountSpec `json:"initialAdmin,omitempty"`
+
+	// Storage configuration for OFBiz data and logs.
+	// +optional
+	Storage StorageSpec `json:"storage,omitempty"`
+
+	// Java Opts.
+	// +kubebuilder:default:"-Xms512m -Xmx1024m"
+	JavaOpts string `json:"javaOpts,omitempty"`
+
+	// AJP port enablement.
+	// +kubebuilder:default:false
+	EnableAJP bool `json:"enableAJP,omitempty"`
+}
+
 // PasswordSource defines a source for a password.
 // Only one of its fields may be set.
 // +kubebuilder:validation:XValidation:rule="has(self.value) != has(self.secretName)", message="exactly one of `value` or `secretName` must be specified"
@@ -49,9 +90,13 @@ type ConfigurationSource struct {
 	ConfigMapName string `json:"configMapName,omitempty"`
 }
 
-// PostgresAdminSpec holds credentials for the operator to manage the database.
-// If this section is provided, the operator will attempt to create the database and user.
-type PostgresAdminSpec struct {
+// DatabaseSpec defines the external database connection details.
+type DatabaseSpec struct {
+	// If provided, the operator will manage the database, user, and permissions.
+	// +optional
+	PostgresAdmin *PostgresAdminSpec `json:"postgresAdmin,omitempty"`
+	// Automatically comissioned if postgresAdmin specified, else should be manually created beforehand.
+
 	// Hostname of the PostgreSQL server to connect to as an administrator.
 	// +kubebuilder:validation:Required
 	Host string `json:"host"`
@@ -60,7 +105,46 @@ type PostgresAdminSpec struct {
 	// +kubebuilder:default=5432
 	Port int32 `json:"port"`
 
-	// The user with privileges to create databases and users (e.g., 'postgres').
+	// SSL Mode for the admin connection (e.g., 'disable', 'require', 'verify-full').
+	// +kubebuilder:default=disable
+	SslMode string `json:"sslMode"`
+
+	// Auto create ofbiz database
+	// +kubebuilder:default=false
+	CreateDB bool `json:"createDB,omitempty"`
+
+	// ofbiz database
+	OfbizDB OfbizDBSpec `json:"ofbizDB,omitempty"`
+
+	// ofbiz OLAP database
+	OfbizOLAPDB OfbizDBSpec `json:"ofbizOLAPDB,omitempty"`
+
+	// ofbiz Tenant database
+	OfbizTenantDB OfbizDBSpec `json:"ofbizTenantDB,omitempty"`
+
+	// Data Load Mode (e.g., 'none', 'seed', 'demo').
+	// +kubebuilder:default=none
+	DataLoad string `json:"dataLoad,omitempty"`
+}
+
+type OfbizDBSpec struct {
+	// Database name.
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+
+	// The user which privileges to own ofbiz database
+	// +kubebuilder:validation:Required
+	User string `json:"user"`
+
+	// The 'password' used for database user.
+	// +kubebuilder:validation:Required
+	Password PasswordSource `json:"password"`
+}
+
+// PostgresAdminSpec holds credentials for the operator to manage the database.
+// If this section is provided, the operator will attempt to create the database and user.
+type PostgresAdminSpec struct {
+	// postgre super user
 	// +kubebuilder:validation:Required
 	User string `json:"user"`
 
@@ -68,51 +152,6 @@ type PostgresAdminSpec struct {
 	// The secret must have a key named 'password'.
 	// +kubebuilder:validation:Required
 	PasswordSecretName string `json:"passwordSecretName"`
-
-	// SSL Mode for the admin connection (e.g., 'disable', 'require', 'verify-full').
-	// +kubebuilder:default=disable
-	SslMode string `json:"sslMode"`
-}
-
-// OfbizSpec defines the desired state of Ofbiz
-type OfbizSpec struct {
-	// Number of desired pods. Defaults to 1.
-	// +kubebuilder:validation:Minimum=1
-	// +kubebuilder:default=1
-	Size int32 `json:"size"`
-
-	// The container image for OFBiz.
-	// +kubebuilder:validation:Required
-	Image string `json:"image"`
-
-	// Configuration for the external PostgreSQL database.
-	// +kubebuilder:validation:Required
-	Database DatabaseSpec `json:"database"`
-
-	// If provided, the operator will manage the database, user, and permissions.
-	// +optional
-	PostgresAdmin *PostgresAdminSpec `json:"postgresAdmin,omitempty"`
-
-	// Web configuration, including SSL.
-	// +optional
-	Web WebSpec `json:"web,omitempty"`
-
-	// Configuration for initial admin account.
-	// +optional
-	InitialAdmin AdminAccountSpec `json:"initialAdmin,omitempty"`
-
-	// Storage configuration for OFBiz data and logs.
-	// +optional
-	Storage StorageSpec `json:"storage,omitempty"`
-}
-
-// DatabaseSpec defines the external database connection details.
-type DatabaseSpec struct {
-	Host     string         `json:"host"`
-	Port     int32          `json:"port"`
-	Name     string         `json:"name"`
-	User     string         `json:"user"`
-	Password PasswordSource `json:"password"`
 }
 
 // WebSpec defines web server configuration.
@@ -124,6 +163,14 @@ type WebSpec struct {
 type AdminAccountSpec struct {
 	Password PasswordSource `json:"password"`
 }
+
+// VolumeMounts for OFBiz
+//      - ./ofbiz/docker-entrypoint-hooks:/docker-entrypoint-hooks:rw
+//      - ./ofbiz/config:/ofbiz/config:rw
+//      - ./ofbiz/lib-extra:/ofbiz/lib-extra:rw
+//      - ./ofbiz/runtime:/ofbiz/runtime:rw
+//      - ./ofbiz/export:/ofbiz/export:rw
+//      - ./ofbiz/plugins:/ofbiz/plugins:rw
 
 // StorageSpec defines the persistence configuration.
 type StorageSpec struct {
